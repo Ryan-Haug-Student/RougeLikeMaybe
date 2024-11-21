4976ee24a6f4ca9ce3fc3587942696ff8939e405
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-11-21 14:29:04 -0500

    changed some room generation settings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // BUY MR LYKE COOKIE IN A COUPLE WEEKS
+
     public int dungeonSize;
 
     int nextRoom;
@@ -17,9 +20,11 @@ public class GameManager : MonoBehaviour
     public GameObject Room2;
     public GameObject Room3;
 
+
+
     void Start()
     {
-        dungeonSize = Random.Range(5, 21);
+        dungeonSize = Random.Range(5, 20);
         nextRoom = Random.Range(1, 4);
         nextPos = Random.Range(1, 4);
 
@@ -28,46 +33,59 @@ public class GameManager : MonoBehaviour
 
     void MakeDungeon()
     {
+        bool movedRoom = false;
+
         for(int i = 0; i < dungeonSize; i++)
         {
-            if (nextPos == 1 && lastRoom != 2)
-            {
-                spawnLoc += new Vector3(-12, 0, 0);
-                lastRoom = 1;
-                Debug.Log("moved left");
-            }
-            else if (nextPos == 2)
-            {
-                spawnLoc += new Vector3(0, 0, 12);
-                lastRoom = 0;
-                Debug.Log("moved forward");
-            }
-            else if (lastRoom != 1)
-            {
-                spawnLoc += new Vector3(12, 0, 0);
-                lastRoom = 2;
-                Debug.Log("moved right");
-            }
-            else
-            {
-                i--;
-            }
+                if (nextPos == 1 && lastRoom != 2)
+                {
+                    spawnLoc += new Vector3(-24, 0, 0);
+                    lastRoom = 1;
+                    movedRoom = true;
 
-            if (nextRoom == 1)
-            {
-                Instantiate(Room1, spawnLoc, Quaternion.identity);
-            }
-            else if (nextRoom == 2)
-            {
-                Instantiate (Room2, spawnLoc, Quaternion.identity);
-            }
-            else
-            {
-                Instantiate ( Room3 , spawnLoc, Quaternion.identity);
-            }
+                    //Debug.Log("moved left");
+                }
+                else if (nextPos == 2)
+                {
+                    spawnLoc += new Vector3(0, 0, 24);
+                    lastRoom = 0;
+                    movedRoom = true;
 
-            nextRoom = Random.Range(1, 4);
-            nextPos = Random.Range(1, 4);
+                    //Debug.Log("moved forward");
+                }
+                else if (lastRoom != 1)
+                {
+                    spawnLoc += new Vector3(24, 0, 0);
+                    lastRoom = 2;
+                    movedRoom = true;
+
+                    //Debug.Log("moved right");
+                }
+                else
+                {
+                    i--;
+                }
+
+                if (movedRoom)
+                {
+                    if (nextRoom == 1)
+                    {
+                        Instantiate(Room1, spawnLoc, Quaternion.identity);
+                    }
+                    else if (nextRoom == 2)
+                    {
+                        Instantiate(Room2, spawnLoc, Quaternion.identity);
+                    }
+                    else
+                    {
+                        Instantiate(Room3, spawnLoc, Quaternion.identity);
+                    }
+                }
+
+                movedRoom = false;
+                nextRoom = UnityEngine.Random.Range(1, 4);
+                nextPos = UnityEngine.Random.Range(1, 4);
         }
+
     }
 }
